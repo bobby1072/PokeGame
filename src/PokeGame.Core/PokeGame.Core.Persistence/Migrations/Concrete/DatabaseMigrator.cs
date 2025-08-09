df39adabd4822a9199bd56ee1772106f6774f827
bobby1072 <bobby.thompson1072@gmail.com>
2025-08-09 23:06:32 +0100

    Removed extra retry

--- src/PokeGame.Core/PokeGame.Core.Persistence/Migrations/Concrete/DatabaseMigrator.cs
+++ src/PokeGame.Core/PokeGame.Core.Persistence/Migrations/Concrete/DatabaseMigrator.cs
@@ -1,5 +1,4 @@
-﻿using BT.Common.Polly.Extensions;
-using EvolveDb;
+﻿using EvolveDb;
 using EvolveDb.Migration;
 using Microsoft.Extensions.Logging;
 using Npgsql;
@@ -11,29 +10,27 @@ namespace PokeGame.Core.Persistence.Migrations.Concrete
     internal sealed class DatabaseMigrator : IMigrator
     {
         private readonly ILogger<DatabaseMigrator> _logger;
-        private readonly DbMigrationSettings _settings;
         private readonly string _connectionString;
+        private readonly string _startVersion;
         public DatabaseMigrator(ILogger<DatabaseMigrator> logger, DbMigrationSettings migrationSettings, string connectionString)
         {
             _logger = logger;
+            _startVersion = migrationSettings.StartVersion;
             _connectionString = connectionString;
-            _settings = migrationSettings;
         }
         public Task Migrate()
         {
             using var connection = new NpgsqlConnection(_connectionString);
             var evolve = new Evolve(connection, msg => _logger.LogInformation(msg))
             {
-                EmbeddedResourceAssemblies = [typeof(DatabaseMigrator).Assembly],
+                EmbeddedResourceAssemblies = new[] { typeof(DatabaseMigrator).Assembly },
                 EnableClusterMode = true,
-                StartVersion = new MigrationVersion(_settings.StartVersion),
+                StartVersion = new MigrationVersion(_startVersion),
                 IsEraseDisabled = true,
                 MetadataTableName = "migrations_changelog",
                 OutOfOrder = true
             };
-
-            _settings.ToPipeline().Execute(() => evolve.Migrate());
-
+            evolve.Migrate();
             return Task.CompletedTask;
         }
     }
