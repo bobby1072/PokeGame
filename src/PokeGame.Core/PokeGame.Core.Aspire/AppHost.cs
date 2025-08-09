4d3d865a95b0be1f61b1dee598d653ff75750acc
bobby1072 <bobby.thompson1072@gmail.com>
2025-08-10 00:56:57 +0100

    Added to aspire

--- src/PokeGame.Core/PokeGame.Core.Aspire/AppHost.cs
+++ src/PokeGame.Core/PokeGame.Core.Aspire/AppHost.cs
@@ -1,4 +1,16 @@
+using Microsoft.Extensions.Configuration;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+
+var useReactServer = builder.Configuration.GetValue<bool>("UseReactServer");
+
+if (useReactServer)
+{
+    builder
+        .AddProject<Projects.PokeGame_Core_React_Server>("PokeGame-Core-React-Server")
+        .WithExternalHttpEndpoints();
+}
 
 builder
     .AddProject<Projects.PokeGame_Core_Api>("PokeGame-Core-Api")
