b10d7fa1e91c84943e70861d70f8fb984a526811
Bobby Thompson <bobbythompson@paypoint.com>
2025-08-11 00:54:37 +0100

    Done so

--- src/phaser-client/src/main.tsx
+++ src/phaser-client/src/main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { App } from "./App.tsx";
 import { AppSettingsContextProvider } from "./common/contexts/AppSettingsContext.tsx";
 import { PokeGameCoreHttpClientContextProvider } from "./common/contexts/PokeGameCoreHttpClientContext.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PokeGameThemeProvider } from "./common/contexts/ThemeContext.tsx";
 import { PokeGameUserContextProvider } from "./common/contexts/PokeGameUserContext.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
@@ -14,7 +15,12 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
                 <PokeGameCoreHttpClientContextProvider>
                     <QueryClientProvider client={new QueryClient()}>
                         <PokeGameUserContextProvider>
-                            <App />
+                            <BrowserRouter>
+                                <Routes>
+                                    <Route path="/" element={<App />} />
+                                    <Route path="/game" element={<App />} />
+                                </Routes>
+                            </BrowserRouter>
                         </PokeGameUserContextProvider>
                     </QueryClientProvider>
                 </PokeGameCoreHttpClientContextProvider>
