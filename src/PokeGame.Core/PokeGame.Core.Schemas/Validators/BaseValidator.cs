c85329f1aeb057a2dca35e96b6b6138a65bf7065
bobby1072 <bobby.thompson1072@gmail.com>
2025-10-02 23:21:20 +0100

    Added them

---
+++ src/PokeGame.Core/PokeGame.Core.Schemas/Validators/BaseValidator.cs
@@ -0,0 +1,22 @@
+﻿using BT.Common.Helpers.Extensions;
+using FluentValidation;
+
+namespace PokeGame.Core.Schemas.Validators;
+
+internal abstract class BaseValidator<T> : AbstractValidator<T>
+{
+    protected static bool IsValidUri(string uri)
+    {
+        try
+        {
+            _ = new Uri(uri);
+
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    protected static bool IsValidEmail(string email) => email.IsValidEmail();
+}
---
+++ src/PokeGame.Core/PokeGame.Core.Schemas/Validators/OwnedPokemonValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace PokeGame.Core.Schemas.Validators;
+
+internal sealed class OwnedPokemonValidator: BaseValidator<OwnedPokemon>
+{
+    public OwnedPokemonValidator()
+    {
+        RuleFor(x => x.ResourceName).NotEmpty().WithMessage("Resource name cannot be empty");
+        RuleFor(x => x.ResourceName).Must(IsValidUri).WithMessage("Resource names must be valid Uris");
+        RuleFor(x => x.PokemonLevel).Must(x => x is > 0 and < 100).WithMessage("Pokemon level must be between 1 - 99");
+        RuleFor(x => x.CurrentExperience).Must(x => x is > 0).WithMessage("Current experience must be greater than 0");
+        RuleFor(x => x.CurrentHp).Must(x => x is >= 0).WithMessage("Current hp cannot be negative");
+        RuleFor(x => x.MoveOneResourceName).NotEmpty().WithMessage("Move one resource name cannot be empty");
+        RuleFor(x => x.MoveOneResourceName).Must(IsValidUri).WithMessage("Resource names must be valid Uris");
+        RuleFor(x => x.MoveTwoResourceName).Must(x => x is not null && IsValidUri(x)).WithMessage("Resource names must be valid Uris");
+        RuleFor(x => x.MoveThreeResourceName).Must(x => x is not null && IsValidUri(x)).WithMessage("Resource names must be valid Uris");
+        RuleFor(x => x.MoveFourResourceName).Must(x => x is not null && IsValidUri(x)).WithMessage("Resource names must be valid Uris");
+    }
+}
