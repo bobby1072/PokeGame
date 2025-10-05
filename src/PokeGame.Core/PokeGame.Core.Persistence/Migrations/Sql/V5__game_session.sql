252e29c04a2b265f2e9f65452d87040f18fa76be
bobby1072 <bobby.thompson1072@gmail.com>
2025-10-05 13:29:58 +0100

    Unique connection id

--- src/PokeGame.Core/PokeGame.Core.Persistence/Migrations/Sql/V5__game_session.sql
+++ src/PokeGame.Core/PokeGame.Core.Persistence/Migrations/Sql/V5__game_session.sql
@@ -2,6 +2,6 @@ CREATE TABLE "public".game_session(
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     game_save_id UUID NOT NULL REFERENCES public.game_save(id) ON DELETE CASCADE ON UPDATE CASCADE,
     user_id UUID NOT NULL REFERENCES public."user"(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    connection_id TEXT NOT NULL,
+    connection_id TEXT NOT NULL UNIQUE,
     started_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now()
 );
