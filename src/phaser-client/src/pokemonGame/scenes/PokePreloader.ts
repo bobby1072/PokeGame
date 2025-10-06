1fc5edf5563b752dd4a05ee7af95d34941260fda
bobby1072 <bobby.thompson1072@gmail.com>
2025-10-06 19:36:06 +0100

    done it

--- src/phaser-client/src/pokemonGame/scenes/PokePreloader.ts
+++ src/phaser-client/src/pokemonGame/scenes/PokePreloader.ts
@@ -6,7 +6,16 @@ export class PokePreloader extends Scene {
         super(PokePreloader.name);
     }
 
+    public preload() {
+        // Load common assets used across all scenes
+        this.load.spritesheet("myPlayer", "/assets/player.png", {
+            frameWidth: 48,
+            frameHeight: 48,
+        });
+    }
+
     public create() {
+        // Start the first scene
         this.scene.start(BasiliaTownScene.name);
     }
 }
