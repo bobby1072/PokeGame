ab3226acee2c5e499318b885716698f34f54c272
bobby1072 <bobby.thompson1072@gmail.com>
2025-11-26 00:47:13 +0000

    Removed circular refd

--- src/PokeGame.Core/PokeGame.Core.Schemas/Game/GameSaveDataActualDeckPokemon.cs
+++ src/PokeGame.Core/PokeGame.Core.Schemas/Game/GameSaveDataActualDeckPokemon.cs
@@ -3,12 +3,8 @@
 public sealed class GameSaveDataActualDeckPokemon: DomainModel<GameSaveDataActualDeckPokemon>
 {
     public required Guid OwnedPokemonId { get; set; }
-    
-    public OwnedPokemon? OwnedPokemon { get; set; }
-
     public override bool Equals(GameSaveDataActualDeckPokemon? other)
     {
-        return other?.OwnedPokemonId == OwnedPokemonId &&
-               OwnedPokemon?.Equals(other.OwnedPokemon) is true or null;
+        return other?.OwnedPokemonId == OwnedPokemonId;
     }
 }
