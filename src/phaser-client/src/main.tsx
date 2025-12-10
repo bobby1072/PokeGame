3941688a37328631cc28aa447a6731b98a780a19
BobbyThompson <bobbythompson@paypoint.com>
2025-12-10 15:22:00 +0000

    Have added test setup stuff

--- src/phaser-client/src/main.tsx
+++ src/phaser-client/src/main.tsx
@@ -1,42 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { AppSettingsContextProvider } from "./common/contexts/AppSettingsContext.tsx";
-import { PokeGameCoreHttpClientContextProvider } from "./common/contexts/PokeGameCoreHttpClientContext.tsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { PokeGameThemeProvider } from "./common/contexts/ThemeContext.tsx";
-import { PokeGameUserContextProvider } from "./common/contexts/PokeGameUserContext.tsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import PokemonPage from "./pokemonGame/PokemonPage";
-import { SignalRGameSessionProvider } from "./common/contexts/SignalRGameSessionContext.tsx";
-import { PokemonDeckProvider } from "./common/contexts/PokemonDeckContext.tsx";
+import { App } from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <PokeGameThemeProvider>
-            <QueryClientProvider client={new QueryClient()}>
-                <AppSettingsContextProvider>
-                    <PokeGameCoreHttpClientContextProvider>
-                        <PokeGameUserContextProvider>
-                            <SignalRGameSessionProvider>
-                                <PokemonDeckProvider>
-                                    <BrowserRouter>
-                                        <Routes>
-                                            <Route
-                                                path="/"
-                                                element={<PokemonPage />}
-                                            />
-                                            <Route
-                                                path="/pokemon"
-                                                element={<PokemonPage />}
-                                            />
-                                        </Routes>
-                                    </BrowserRouter>
-                                </PokemonDeckProvider>
-                            </SignalRGameSessionProvider>
-                        </PokeGameUserContextProvider>
-                    </PokeGameCoreHttpClientContextProvider>
-                </AppSettingsContextProvider>
-            </QueryClientProvider>
-        </PokeGameThemeProvider>
+        <App />
     </React.StrictMode>
 );
