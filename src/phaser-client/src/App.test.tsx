c7182031c7ec1d0baecb3379eb413710dd2b420e
bobby1072 <bobby.thompson1072@gmail.com>
2025-12-11 00:01:23 +0000

    Tests run correct

---
+++ src/phaser-client/src/App.test.tsx
@@ -0,0 +1,14 @@
+import { screen, waitFor } from "../test/utils/test-utils";
+import { describe, expect, it } from "vitest";
+import { App } from "./App";
+import { render } from "@testing-library/react";
+
+describe(App.name, () => {
+    it("Simple top level render test", async () => {
+        render(<App />);
+
+        await waitFor(() => {
+            expect(screen.getByTitle("PokeGame")).toBeInTheDocument();
+        });
+    });
+});
--- src/phaser-client/test/setup.js
+++ src/phaser-client/test/setup.js
@@ -1,7 +1,43 @@
-import { afterEach } from 'vitest'
-import { cleanup } from '@testing-library/react'
-import '@testing-library/jest-dom/vitest'
+import { afterEach, vi } from "vitest";
+import { cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
+import { mockServer } from "./mocks/mockServer";
+
+mockServer.listen();
+
+// Mock phaser3spectorjs module
+vi.mock("phaser3spectorjs", () => {});
+
+// Mock canvas for Phaser
+HTMLCanvasElement.prototype.getContext = () => {
+    return {
+        fillStyle: "",
+        fillRect: () => {},
+        clearRect: () => {},
+        getImageData: () => ({ data: [] }),
+        putImageData: () => {},
+        createImageData: () => [],
+        setTransform: () => {},
+        drawImage: () => {},
+        save: () => {},
+        restore: () => {},
+        beginPath: () => {},
+        moveTo: () => {},
+        lineTo: () => {},
+        closePath: () => {},
+        stroke: () => {},
+        translate: () => {},
+        scale: () => {},
+        rotate: () => {},
+        arc: () => {},
+        fill: () => {},
+        measureText: () => ({ width: 0 }),
+        transform: () => {},
+        rect: () => {},
+        clip: () => {},
+    };
+};
 
 afterEach(() => {
-  cleanup();
-})
+    cleanup();
+});
