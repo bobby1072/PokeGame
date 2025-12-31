f206107e6afd0863371aaaa8d7dceba484d236cd
BobbyThompson <bobbythompson@paypoint.com>
2025-12-31 14:24:14 +0000

    Adding more to details

--- src/PokeGame.Core/PokeGame.Core.Schemas/Game/PokemonRelated/OwnedPokemonInnerDetails.cs
+++ src/PokeGame.Core/PokeGame.Core.Schemas/Game/PokemonRelated/OwnedPokemonInnerDetails.cs
@@ -8,14 +8,16 @@ public sealed class OwnedPokemonInnerDetails: DomainModel<OwnedPokemonInnerDetai
     public int Height { get; set; }
     public int Weight { get; set; }
     public required PokemonSpriteDetails Sprites { get; set; }
-    // public required List<PokemonStat> Stats { get; set; }
-    // public required List<PokemonType> Types { get; set; }
+    public required List<PokemonStatDetails> Stats { get; set; }
+    public required List<PokemonTypeDetails> Types { get; set; }
 
     public override bool Equals(OwnedPokemonInnerDetails? other)
     {
         return BaseExperienceFromDefeating == other?.BaseExperienceFromDefeating &&
                Height == other.Height &&
                Weight == other.Weight &&
-               Sprites.Equals(other.Sprites);
+               Sprites.Equals(other.Sprites) &&
+               Stats.SequenceEqual(other.Stats) &&
+               Types.SequenceEqual(other.Types);
     }
 }
